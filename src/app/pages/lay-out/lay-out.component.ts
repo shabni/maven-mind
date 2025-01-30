2d1f8c79a52e6ffe8421623145eaee68d5bfe105
Ahsan Ahmad <ahsan.ahmad@ovada.com>
2025-01-30 13:04:51 +0500

    add layout

--- src/app/pages/lay-out/lay-out.component.ts
+++ src/app/pages/lay-out/lay-out.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-lay-out',
   templateUrl: './lay-out.component.html',
   styleUrls: ['./lay-out.component.scss']
 })
-export class LayOutComponent implements OnInit {
+export class LayOutComponent {
 
-  constructor() { }
+  userName = "Guest"; // Replace with actual authentication logic
 
-  ngOnInit(): void {
+  logout() {
+    console.log("Logout clicked");
+    // Implement logout logic here
   }
 
 }
