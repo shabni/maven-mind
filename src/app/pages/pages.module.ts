66e089ec82435bfe228561f7e9f6083836a2d168
Ahsan Ahmad <ahsan.ahmad@ovada.com>
2025-01-30 13:04:35 +0500

    add base

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,12 +4,16 @@ import { CommonModule } from '@angular/common';
 import { PagesRoutingModule } from './pages-routing.module';
 import { PagesComponent } from './pages.component';
 import { LayOutComponent } from './lay-out/lay-out.component';
+import { HeaderComponent } from './lay-out/header/header.component';
+import { SideBarComponent } from './lay-out/side-bar/side-bar.component';
 
 
 @NgModule({
   declarations: [
     PagesComponent,
-    LayOutComponent
+    LayOutComponent,
+    HeaderComponent,
+    SideBarComponent
   ],
   imports: [
     CommonModule,
