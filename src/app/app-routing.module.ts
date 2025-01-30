32ae0feff1f002d611e964e44ac20da4002c8978
Ahsan Ahmad <ahsan.ahmad@ovada.com>
2025-01-30 10:02:00 +0500

    add auth module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: '', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) },
+  { path: '**', redirectTo: 'auth/login', pathMatch: 'full' } // Default fallback
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
