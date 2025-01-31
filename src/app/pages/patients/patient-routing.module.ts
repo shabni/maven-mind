88e8296b2d8fb3793af97dd5d89552c35f61bbf5
Ahsan Ahmad <ahsan.ahmad@ovada.com>
2025-01-31 16:36:14 +0500

    add form component

--- src/app/pages/patients/patient-routing.module.ts
+++ src/app/pages/patients/patient-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PatientListComponent } from './patient-list/patient-list.component';
+import { PatientFormComponent } from './patient-form/patient-form.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'list', pathMatch: 'full' },
   { path: 'list', component: PatientListComponent },
-  { path: 'add', component: PatientListComponent },
+  { path: 'add', component: PatientFormComponent },
 ];
 
 @NgModule({
