1a6b5ca54ecd1c6072b887633017df54c27fb2b3
Ahsan Ahmad <ahsan.ahmad@ovada.com>
2025-01-31 10:44:47 +0500

    Add breadscrumb

---
+++ src/app/pages/breadcrumbs/breadcrumbs.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
+
+@Component({
+  selector: 'app-breadcrumbs',
+  templateUrl: './breadcrumbs.component.html',
+  styleUrls: ['./breadcrumbs.component.scss']
+})
+export class BreadcrumbsComponent implements OnInit {
+
+  breadcrumbs: { label: string; url: string }[] = [];
+
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.generateBreadcrumbs();
+      });
+
+    this.generateBreadcrumbs();
+  }
+
+  generateBreadcrumbs(): void {
+    this.breadcrumbs = [];
+    let url = '';
+    let currentRoute = this.route.root;
+
+    while (currentRoute.firstChild) {
+      currentRoute = currentRoute.firstChild;
+      if (currentRoute.snapshot.url.length) {
+        const pathSegment = currentRoute.snapshot.url.map(segment => segment.path).join('/');
+        url += `/${pathSegment}`;
+        this.breadcrumbs.push({ label: pathSegment, url: url });
+      }
+    }
+
+    console.log(this.breadcrumbs, "=====>>>");
+  }
+}
