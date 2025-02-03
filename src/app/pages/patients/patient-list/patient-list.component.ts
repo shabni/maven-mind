e2e4f44f2fdf98a158b79b5cdb73132d042ed40c
Ahsan Ahmad <ahsan.ahmad@ovada.com>
2025-02-03 15:46:14 +0500

    patients list filter

--- src/app/pages/patients/patient-list/patient-list.component.ts
+++ src/app/pages/patients/patient-list/patient-list.component.ts
@@ -1,9 +1,29 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-patient-list',
   templateUrl: './patient-list.component.html',
-  styleUrls: ['./patient-list.component.scss']
+  styleUrls: ['./patient-list.component.scss'],
+  animations: [
+    trigger('collapseAnimation', [
+      state('collapsed', style({
+        height: '0',
+        overflow: 'hidden',
+        opacity: '0',
+        margin: '0',
+      })),
+      state('expanded', style({
+        height: '*',
+        opacity: '1',
+        margin: '*',
+      })),
+      transition('collapsed <=> expanded', [
+        animate('300ms ease-out')
+      ]),
+    ])
+  ]
 })
 export class PatientListComponent implements OnInit {
 
@@ -13,6 +33,10 @@ export class PatientListComponent implements OnInit {
     total:100
   }
   perPageOptions = [10, 25, 50, 100];
+
+  isCollapsed = true;
+  filterForm!: FormGroup;
+
 
   columns = [
     { name: 'Patient ID', prop: 'id' },
@@ -38,9 +62,31 @@ export class PatientListComponent implements OnInit {
     { id: 9, name: 'Amal', email: 'cyzolivih@mailinator.com', contact: '+1 (121) 416-6864', status: '', createdBy: 'ajmal', createdDate: '2024-08-12', updatedBy: 'Admin', updatedDate: '2024-08-12' },
     { id: 8, name: 'Merritt update', email: 'lakobebyupdate@mailinator.com', contact: '+1 (729) 274-3054', status: '', createdBy: 'Admin', createdDate: '2024-08-09', updatedBy: 'Admin', updatedDate: '2024-08-12' },
   ];
-  constructor() { }
+  constructor(private fb: FormBuilder,) {
+    this.buildForm()
+  }
 
   ngOnInit(): void {
+
+  }
+
+  buildForm(){
+    this.filterForm = this.fb.group({
+      ssn: [''],
+      patient_name: [''],
+      date_of_birth: [''],
+      created_at: [''],
+      updated_at: [''],
+      created_by: [''],
+      updated_by: [''],
+      patient_id: [''],
+      email: [''],
+      cell_phone_no: [''],
+      address: [''],
+      ssn3: ['', [Validators.minLength(3), Validators.maxLength(3)]],
+      ssn2: ['', [Validators.minLength(2), Validators.maxLength(2)]],
+      ssn4: ['', [Validators.minLength(4), Validators.maxLength(4)]],
+    });
   }
 
   setPage(pageInfo: any) {
@@ -54,5 +100,21 @@ export class PatientListComponent implements OnInit {
   getTotalPages(): number {
     return Math.ceil(this.page.total / this.page.perPage);
   }
+
+  onSelectFilters(): void {
+    // this.pActivePage = 1;
+    // this.pOffset = 0;
+    // this.pTotal = 0;
+    // this.patientRows = [];
+    // this.showPatientForm = true;
+    // this.filterButtonClicked = true;
+    // this.listingApiCalled = true;
+    // this.getAllPatients(null);
+  }
+
+  resetForm(){}
+
+  onResetFilters(){}
+
 
 }
