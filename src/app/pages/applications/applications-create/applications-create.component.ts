ce16dc6ae4752b0bc0085d957dd5792fde2f90a7
Ahsan Ahmad <ahsan.ahmad@ovada.com>
2025-02-18 11:09:10 +0500

    create applications

---
+++ src/app/pages/applications/applications-create/applications-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-applications-create',
+  templateUrl: './applications-create.component.html',
+  styleUrls: ['./applications-create.component.scss']
+})
+export class ApplicationsCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
