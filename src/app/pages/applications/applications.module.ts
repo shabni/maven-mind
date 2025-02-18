48573bfcd860fcceef495441effd7d6cbe4438f5
Ahsan Ahmad <ahsan.ahmad@ovada.com>
2025-02-18 11:56:23 +0500

    create application

--- src/app/pages/applications/applications.module.ts
+++ src/app/pages/applications/applications.module.ts
@@ -4,6 +4,10 @@ import { CommonModule } from '@angular/common';
 import { ApplicationsRoutingModule } from './applications-routing.module';
 import { ApplicationsComponent } from './applications.component';
 import { ApplicationsCreateComponent } from './applications-create/applications-create.component';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 
 @NgModule({
@@ -13,7 +17,11 @@ import { ApplicationsCreateComponent } from './applications-create/applications-
   ],
   imports: [
     CommonModule,
-    ApplicationsRoutingModule
+    ApplicationsRoutingModule,
+    NgxDatatableModule,
+    ReactiveFormsModule,
+    NgSelectModule,
+    CollapseModule.forRoot()
   ]
 })
 export class ApplicationsModule { }
